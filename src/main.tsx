c8616a7822def25acfbc403a864ad0b9dc1ada34
KreedAc <giovanniavignone@gmail.com>
2026-01-07 13:03:19 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+if (window.location.hostname === 'wakfujobcalculator.bolt.host') {
+  window.location.href = 'https://wakfujobcalculator.com' + window.location.pathname + window.location.search + window.location.hash;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
