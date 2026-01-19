a026f4a9fd1a4318070f9ca595075cb82a26e6d4
KreedAc <giovanniavignone@gmail.com>
2026-01-19 15:16:26 +0100

    Updated Sublimations.tsx

--- src/components/Sublimations.tsx
+++ src/components/Sublimations.tsx
@@ -260,7 +260,7 @@ const filteredRunes = useMemo(() => {
                     {rune.obtenation && rune.obtenation.name && (
                       <>
                         <LocalImage
-                          src={rune.obtenation.localIcon.replace('/icons/', '/data/icons/')}
+                          src={rune.obtenation.localIcon}
                           alt=""
                           className="w-7 h-7 rounded bg-slate-700"
                         />
